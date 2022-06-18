4e8cad7a1497db8cfa3cfd2aebbc4c51bf89f5cf
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-06-18 10:00:02 +0530

    Initial commit

---
+++ src/com/niit/fileinput/FileInputDemo.java
@@ -0,0 +1,42 @@
+package com.niit.fileinput;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class FileInputDemo {
+    public static void readFile(String fileName){
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(fileName);//READS FILES IN BYTES
+            int readByte = 0;
+            int count = 0;
+            System.out.println("fileInputStream.available() = " + fileInputStream.available());
+           /* while (fileInputStream.available() != 0) {
+                System.out.print(fileInputStream.read());
+
+            }
+            System.out.println();
+            System.out.println("count = " + count);*/
+            byte[] b = new byte[fileInputStream.available()];
+            //System.out.println(Arrays.toString(b));
+            int readBlockOfBytes = fileInputStream.read(b);
+            System.out.println("readBlockOfBytes = " + readBlockOfBytes);
+            //System.out.println(Arrays.toString(b));
+            for (byte b1 : b) {
+                System.out.print((char)b1);
+            }
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile("resource/onemore/xyz.txt");
+    }
+}
